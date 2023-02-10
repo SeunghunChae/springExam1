fbed7a03d16714cf2c9ad0b064d7de8b1638729c
이상민 <33484628+SM922@users.noreply.github.com>
2023-02-10 17:40:48 +0900

    주가동향 페이지 에러 수정

--- src/main/resources/static/assets/js/tables.js
+++ src/main/resources/static/assets/js/tables.js
@@ -3,15 +3,6 @@
 
 window.onload = function(){
 
-    // $.ajax({
-    //     url: 'localhost:8080/api/theme/top4/all',
-    //     type: 'GET',
-    //     data: {},
-    //     success: function(result) {
-    //         console.log(result);
-    //     }
-    // })
-
     var settings = {
         "url": "http://localhost:8080/api/theme/top4/all",
         "method": "GET",
@@ -26,6 +17,8 @@ window.onload = function(){
       $.ajax(settings).done(function (data) {
         console.log(data);
 
+        $("table_1_name").text("dddd");
+
         for(var i = 0; i < 6; i++){
             var temp = data;
             var table = '#table_'+(i+1);
@@ -35,34 +28,42 @@ window.onload = function(){
             console.log(nameTemp);
             //$(nameTemp).text = "ddddd";
 
-
             //등락률에 따른 데이터 색상 변경로직
             var SelectRows = "TableBody" + (i+1);
-            //console.log(SelectRows);
-            var rows = document.getElementById(eval("'"+SelectRows+"'")).getElementsByTagName("tr");
+            var rows = document.getElementById(eval("'"+SelectRows+"'")).getElementsByTagName("tr");    
 
-            $("table_1_name").text("dddd");
-
-            for(var j =0; j<3; j++){        
+            
+            for(var j =0; j<4; j++){        
                 var table_data="";
                 table_data+="<tr>";    
                 table_data+="<td>"+(j+1)+"</td>"
                 table_data+="<td>"+temp.themeList[i].top4List[j].stockName+"</td>"
                 table_data+="<td>"+temp.themeList[i].top4List[j].stockCode+"</td>"
-                table_data+="<td>"+temp.themeList[i].top4List[j].stockUpAndDown+"</td>"
+                if(temp.themeList[i].top4List[j].stockUpAndDown >0){
+                    table_data+="<td>"+temp.themeList[i].top4List[j].stockUpAndDown+"&#9650;"+"</td>"
+                }
+                else if(temp.themeList[i].top4List[j].stockUpAndDown == 0){
+                    table_data+="<td>"+temp.themeList[i].top4List[j].stockUpAndDown+"&#9644;"+"</td>"
+                }
+                else if(temp.themeList[i].top4List[j].stockUpAndDown < 0){
+                    table_data+="<td>"+temp.themeList[i].top4List[j].stockUpAndDown+"&#9660;"+"</td>"
+                }        
                 table_data+="<td>"+temp.themeList[i].top4List[j].stockPrice+"</td>"
                 table_data+="</tr>";
                            
-                console.log(table);
                 $(table).append(table_data); 
+                //console.log(table_data);
             }
 
             for(var j = 0; j < rows.length; j++){
                 var cells = rows[j].getElementsByTagName("td"); 
-                var UpsDowns = cells[2].firstChild.data;
+                var UpsDowns = cells[3].firstChild.data;
+                console.log(UpsDowns);
+
                 if(UpsDowns > 0){
                     cells[3].style.color="red";
-                    cells[4].style.color="red";     
+                    cells[4].style.color="red";   
+                    
                 }
                 else if(UpsDowns == 0){
                     cells[3].style.color="black";
@@ -75,37 +76,12 @@ window.onload = function(){
             
             }
             
-            //console.log(temp);
+            console.log(temp);
             
        
         }
       });
 
-
-    //   for(var j = 0; j < 6; j++){
-
-    //     var SelectRows = "TableBody" + (j+1);
-    //     //console.log(SelectRows);
-    //     var rows = document.getElementById(eval("'"+SelectRows+"'")).getElementsByTagName("tr");
-
-    //     for(var i = 0; i < rows.length; i++){
-    //         var cells = rows[i].getElementsByTagName("td"); 
-    //         var UpsDowns = cells[2].firstChild.data;
-    //         if(UpsDowns > 0){
-    //             cells[2].style.color="red";
-    //             cells[3].style.color="red";     
-    //         }
-    //         else if(UpsDowns == 0){
-    //             cells[2].style.color="black";
-    //             cells[3].style.color="black";
-    //         }
-    //         else if(UpsDowns < 0){
-    //             cells[2].style.color="blue";
-    //             cells[3].style.color="blue";
-    //         }
-        
-    //     }
-    // }
 
 
 
@@ -119,6 +95,3 @@ window.onload = function(){
 
 }
 
-
-var script = document.createElement('script');
- script.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
