f15fbe551b24360472141c6a1eeb0e36c785b797
SeunghunChae <48344785+SeunghunChae@users.noreply.github.com>
2023-02-12 23:50:26 +0900

    클릭 예제 업글

--- 파이썬(크롤링, lstm 소스)/파이썬크롤링/클릭 예제 (네이버로그인 시도).py
+++ 파이썬(크롤링, lstm 소스)/파이썬크롤링/클릭 예제 (네이버로그인 시도).py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome("c:/chromedriver.exe") 
 
@@ -8,15 +9,17 @@ browser = webdriver.Chrome("c:/chromedriver.exe")
 browser.get("http://www.naver.com")
 
 # 2. 네이버 로그인 버튼 클릭
-elem = browser.find_element_by_class_name("link_login")
+elem = browser.find_element(By.CLASS_NAME, 'link_login')
 elem.click()
 
+time.sleep(1)
 # 3. 아이디, 비밀번호 입력
 
-browser.find_element_by_id("id").send_keys("id")
-browser.find_element_by_id("pw").send_keys("password")
+browser.find_element(By.ID, "id").send_keys("shchae822")
+browser.find_element(By.ID, "pw").send_keys("gnslgnsl1#")
 
 # 4. 로그인 버튼 클릭
-browser.find_element_by_id("log.login").click()
+browser.find_element(By.ID, "log.login").click()
 
 time.sleep(3) # 동시 처리되는 것을 막기 위해 3초를 쉰다.
+#browser.close()
