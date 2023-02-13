7e1bd3f61bd30b921a2488993e7ef53bfb061849
SeunghunChae <48344785+SeunghunChae@users.noreply.github.com>
2023-02-13 13:51:15 +0900

    파이썬 크롤링 가져옴

---
+++ 파이썬크롤링/크롤링.py
@@ -0,0 +1,102 @@
+from bs4 import BeautifulSoup
+import requests
+import mysql.connector
+
+#db connection dictionary
+config={
+    'user':'root',
+    'password':'cbk0501',
+    'host':'3.34.141.19',
+    'database':'hackathon',
+    'port':'3306'
+}
+
+def getConn():
+    connection=mysql.connector.connect(**config)
+    return connection
+
+
+def create_table(tName):
+    conn=getConn()
+    cur=conn.cursor()
+    #tsmt='create table ' +tName+' (name varchar(30),price varchar(256),time varchar(256),stock_code varchar(10))'
+    tsmt='create table ' +tName+' (tema_name varchar(50),net_change varchar(30),no_stock varchar(20),3days_net_change varchar(30))'
+
+    cur.execute(tsmt)
+    conn.commit()
+    conn.close()
+
+def insert_a(tName,data):
+    conn=getConn()
+    cur=conn.cursor()
+    tsmt='insert into '+tName+' values(%s, %s, %s, %s)'
+    cur.execute(tsmt,data)
+    conn.commit()
+    conn.close()
+
+def select_a(tName):
+    conn=getConn()
+    cur=conn.cursor()
+    tsmt='select * from '+tName
+    cur.execute(tsmt)
+    rs=cur.fetchall()
+    for i in rs:
+        print(i)
+    #conn.commit()
+    conn.close()
+
+
+
+#크롤링 시작
+
+#종목코드
+code='054630'
+stockname='에이디칩스'
+    
+#삼성전자 url
+url = 'https://finance.naver.com/item/sise_day.naver?code='+code
+html=requests.get(url, headers={'User-agent' : 'Mozilla/5.0'}).text
+bs=BeautifulSoup(html, "lxml")
+
+pgrr = bs.find('td', class_='pgRR')
+#pgrr.a['href'].split('=')[-1] 마지막 페이지 번호
+tdate = bs.find_all('span', class_='gray03')
+tdate.reverse()
+
+tprice = bs.find_all('span','tah p11')
+price=[]
+i=0
+for val in tprice:
+    if i%5==0:
+        price.append(val.text)
+    i+=1
+price.reverse()
+
+date=[]
+for i in tdate:
+    date.append(i.text)
+
+strdate=''
+strprice=''
+
+
+for i in range(10):
+    if i!=9:
+        strdate+=date[i]+'|'
+        strprice+=price[i]+'|'
+    else:
+        strdate+=date[i]
+        strprice+=price[i]
+print(strdate)
+print(strprice)
+
+
+data=(stockname,strprice,strdate,code)
+
+#db process
+
+#create_table("dummydata")
+
+#insert_a("dummydata",data)
+
+create_table("temaScraping")
---
+++ 파이썬크롤링/키움테마주 (selenium headless option).py
@@ -0,0 +1,134 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+import requests
+import mysql.connector
+
+import time
+
+#db connection dictionary
+config={
+    'user':'root',
+    'password':'cbk0501',
+    'host':'3.34.141.19',
+    'database':'hackathon',
+    'port':'3306'
+}
+
+def getConn():
+    connection=mysql.connector.connect(**config)
+    return connection
+
+
+def create_table(tName):
+    conn=getConn()
+    cur=conn.cursor()
+    #tsmt='create table ' +tName+' (name varchar(30),price varchar(256),time varchar(256),stock_code varchar(10))'
+    tsmt='create table ' +tName+' (no int, tema_name varchar(50), stock_name varchar(50), price varchar(20)'+\
+          ',net_change varchar(20),  updown_rate varchar(20), volume varchar(20), ask_price varchar(20), selling_ballance varchar(20)'+\
+          ',bid_price varchar(20), buy_ballance varchar(20), 3days_net_change varchar(20))'
+
+    cur.execute(tsmt)
+    conn.commit()
+    conn.close()
+
+def insert_a(tName,data):
+    conn=getConn()
+    cur=conn.cursor()
+    tsmt='insert into '+tName+' values(%s, %s, %s, %s, %s,%s,%s,%s)'
+    cur.execute(tsmt,data)
+    conn.commit()
+    conn.close()
+
+def select_a(tName):
+    conn=getConn()
+    cur=conn.cursor()
+    tsmt='select * from '+tName
+    cur.execute(tsmt)
+    rs=cur.fetchall()
+    for i in rs:
+        print(i)
+    #conn.commit()
+    conn.close()
+
+def delete_table(tName):
+    conn=getConn()
+    cur=conn.cursor()
+    tsmt='DELETE FROM ' +tName
+    cur.execute(tsmt)
+    conn.commit()
+    conn.close()
+
+
+
+#create_table('stockintema')
+
+
+
+#create_table('temascraping')
+
+#크롤링 시작
+url = "https://www1.kiwoom.com/h/invest/research/VThemaGroupView"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+service = Service('c:\chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(url)
+
+#1초 기다려야 js를 읽어올 수 있음
+time.sleep(1)
+
+#driver을 최대 10초까지 기다림
+#wait = WebDriverWait(driver, 10)
+#css셀렉터를 통해 table.kwGridHead.tb-kw-grid이 나올때까지 기다림
+#table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.kwGridHead.tb-kw-grid")))
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+
+table = soup.find("table", {"class": "kwGridHead tb-kw-grid"})
+
+#10개만 읽어옴
+i=0
+
+data=[]
+for row in table.find_all("tr"):
+    #print("row.find_all(td)")
+    #print(row.find_all("td"))
+    #각 행마다
+    rowdata=[]
+    for cell in row.find_all("td"):
+        if str(cell.contents[0]).find("rate-increase") !=-1 :
+            #print(cell.contents[0])
+            rowdata.append("+"+cell.text)
+        elif str(cell.contents[0]).find("rate-decrease") !=-1:
+            #print(cell.contents[0])
+            rowdata.append("-"+cell.text)
+        else:
+            rowdata.append(cell.text)
+    i+=1
+    data.append(rowdata)
+    if i==31:
+        break
+
+
+driver.quit()
+
+
+delete_table('temascraping')
+
+#크롤링 끝 items는 인덱스가 1번부터 시작함
+for i in range(1,len(data)):
+    #stock_inc와 desc는 부호제거
+    rowdata=(i,data[i][0],data[i][2],data[i][1],data[i][5],data[i][3][1:],data[i][4][1:],data[i][6])
+    insert_a('temascraping', rowdata)
+
+
+
