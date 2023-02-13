df77f443382228bcc602b752b57acb2f2178150a
이상민 <33484628+SM922@users.noreply.github.com>
2023-02-13 11:08:50 +0900

    모달창 추가
    모달 바깥영역 클릭시 종료

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -110,6 +110,21 @@ window.onload = function(){
    }
 
 
+   const body = document.querySelector('body');
+   const modal = document.getElementById("modal");
+   const modalbody = document.getElementById("modal_body");
+   const btnModal = document.getElementById("btn-modal");
+
+   modal.style.display = "flex";
+   
+   modal.addEventListener("click", e => {
+       const evTarget = e.target
+       if(evTarget.classList.contains("modal")) {
+           modal.style.display = "none"
+       }
+   })
+
+
 
 
 
